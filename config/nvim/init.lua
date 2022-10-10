86d2271305acbf47911da65e32dd9dec07713ab1
intcup <etini.artur@outlook.com>
2022-10-09 23:34:05 -0500

    Changes to Helix, Fish & Alacritty

--- config/nvim/init.lua
+++
@@ -1,8 +0,0 @@
-vim.opt.number = true
-vim.opt.termguicolors = true
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
-vim.g.moonflyTransparent = true
-vim.cmd("color moonfly")
-
-require('gitsigns').setup()
